85a679a77a63125d32ec2c29d4fd8dfa690ee53f
rendyap100 <70360955+rendyap100@users.noreply.github.com>
2024-04-05 10:54:22 +0700

    Add files via upload

---
+++ ssh/about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[45;1;39m          ⇱ SC PREMIUM ⇲                        \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\033[0;35m ✘\033[0m For Debian 10 64 bit                          "
+echo -e "\033[0;35m ✘\033[0m For Ubuntu 18.04 & Ubuntu 20.04 64 bit        "
+echo -e "\033[0;35m ✘\033[0m For VPS with KVM and VMWare virtualization    "
+echo -e "\033[0;35m ✘\033[0m Build Up By ARLAN STORE                       "
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[45;1;39m          ⇱ THANKS TO ⇲                         \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\033[0;35m ❀\033[0m Allah SWT                                     "
+echo -e "\033[0;35m ❀\033[0m ARLAN STORE                                     "
+echo -e "\033[0;35m ❀\033[0m Buy: 083805933853                                        "
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[45;1;39m          ⇱ ARLAN STORE ⇲                       \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
